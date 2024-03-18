3644a27ce468f63bf39fc70404a64f8923757e56
Sinri Edogawa <e.joshua.s.e@gmail.com>
2024-03-18 11:37:37 +0800

    Chapter I, Page 2.

--- src/main/java/io/github/sinri/drydock/lesson/caravel/Yubari.java
+++ src/main/java/io/github/sinri/drydock/lesson/caravel/Yubari.java
@@ -1,42 +1,58 @@
 package io.github.sinri.drydock.lesson.caravel;
 
 import io.github.sinri.drydock.naval.melee.Caravel;
-import io.github.sinri.keel.cache.KeelAsyncEverlastingCacheInterface;
 import io.github.sinri.keel.facade.async.KeelAsyncKit;
+import io.github.sinri.keel.mysql.KeelMySQLDataSourceProvider;
+import io.github.sinri.keel.mysql.NamedMySQLDataSource;
+import io.github.sinri.keel.mysql.exception.KeelSQLResultRowIndexError;
+import io.github.sinri.keel.mysql.statement.AnyStatement;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 
 import javax.annotation.Nonnull;
-
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.github.sinri.keel.facade.KeelInstance.Keel;
-import static io.github.sinri.keel.helper.KeelHelpersInterface.KeelHelpers;
 
 public class Yubari extends Caravel {
+    private NamedMySQLDataSource<YubariMySQLConnection> dataSource;
 
-    private AtomicInteger counter;
 
     @Nonnull
     @Override
     protected Future<Void> prepareDataSources() {
-        counter = new AtomicInteger(0);
-        return Future.succeededFuture();
+        dataSource = KeelMySQLDataSourceProvider.initializeNamedMySQLDataSource(YubariMySQLConnection.dataSourceName, YubariMySQLConnection::new);
+        getLogger().info("yubari mysql config", dataSource.getConfiguration().toJsonObject());
+        getLogger().info("ver: " + dataSource.getFullVersionRef());
+        return KeelAsyncKit.sleep(1_000L);
+        //return Future.succeededFuture();
     }
 
     @Override
     protected Future<Void> launchAsCaravel() {
-        return KeelAsyncKit.repeatedlyCall(routineResult -> {
-                    counter.incrementAndGet();
-                    getLogger().info("counting: "+counter.get());
-                    if (counter.get() > 3) {
-                        routineResult.stop();
-                        return Future.succeededFuture();
-                    } else {
-                        return KeelAsyncKit.sleep(1000L);
-                    }
+        return dataSource.withConnection(yubariMySQLConnection -> {
+                    return KeelAsyncKit.repeatedlyCall(routineResult -> {
+                        return AnyStatement.select(selectStatement -> selectStatement
+                                        .columnWithAlias("rand()", "r")
+                                )
+                                .execute(yubariMySQLConnection)
+                                .compose(resultMatrix -> {
+                                    try {
+                                        JsonObject firstRow = resultMatrix.getFirstRow();
+                                        Double r = firstRow.getDouble("r");
+                                        getLogger().info("r: " + r);
+                                        if (r > 0.8) {
+                                            getLogger().notice("Finally!");
+                                            return Future.succeededFuture();
+                                        } else {
+                                            return KeelAsyncKit.sleep(1_000L);
+                                        }
+                                    } catch (KeelSQLResultRowIndexError e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                });
+                    });
                 })
                 .onSuccess(v -> {
                     getLogger().notice(Keel.config("yubari.hello"));
